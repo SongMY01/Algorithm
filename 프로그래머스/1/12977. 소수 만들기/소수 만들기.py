fc97511fbafd16e8091918fde21c7a97ef711882
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-04-19 23:06:18 +0900

    [level 1] Title: 소수 만들기, Time: 26.46 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+def solution(nums):
+    count = 0
+    
+    for comb in combinations(nums, 3):
+        total = sum(comb)
+        if is_prime(total):
+            count += 1
+            
+    return count
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
