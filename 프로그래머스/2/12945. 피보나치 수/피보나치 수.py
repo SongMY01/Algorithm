39f8a3781c3e555a8a427987c78800a0c5db316e
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-04-20 13:05:22 +0900

    [level 2] Title: 피보나치 수, Time: 499.12 ms, Memory: 456 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    mod = 1234567
+    
+    fib = [0] * (n + 1)
+    fib[0] = 0
+    fib[1] = 1
+    for i in range(2,n+1):
+        fib[i] = fib[i-2] + fib[i-1]
+    
+    answer = fib[n] % mod
+    
+    
+    return answer
