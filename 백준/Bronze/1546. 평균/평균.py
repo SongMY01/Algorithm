138aa8b3b76fb3f87fe9f28ef9d0b439da9bfd95
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-11-15 20:40:36 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,5 @@
 n = int(input())
 nums = list(map(int,input().split()))
-
-result = 0
 m = max(nums)
-for num in nums:
-    result += num/m*100
-result = result/n
-print(result)
+sum = sum(nums)
+print(sum/m*100/n)
