ce588198c5531cf83b88d98b8aa76f7bc7075c6e
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-11-16 23:29:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 624 ms, Memory: 67048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,29 +1,34 @@
 import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+
 graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
 
 # 그래프 입력
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
-    graph[v].append(u)
+    graph[v].append(u)  # 무방향 그래프
 
-# DFS 함수 정의
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+visited = [False] * (n + 1)
 
-# 연결 요소 세기
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        cur = queue.popleft()
+        for nxt in graph[cur]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                queue.append(nxt)
+
 count = 0
 for i in range(1, n + 1):
     if not visited[i]:
-        dfs(i)
+        bfs(i)
         count += 1
 
 print(count)
