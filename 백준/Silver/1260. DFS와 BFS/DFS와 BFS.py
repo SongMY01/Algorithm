3e76e126a1e13abf20faf69ba75882571ad13356
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-11-17 16:51:54 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35424 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,45 +1,45 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-# 입력
 n, m, v = map(int, input().split())
 
-# 그래프 초기화
-graph = [[] for _ in range(n + 1)]
+graph = [[] for _ in range(n+1)]
 
-# 간선 정보 저장 (무방향)
+# 그래프 입력
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# 정점 번호 작은 순 방문을 위해 정렬
-for i in range(1, n + 1):
+# 인접 리스트 정렬 (DFS를 작은 번호부터 방문하게 만들기 위해)
+for i in range(1, n+1):
     graph[i].sort()
 
-# DFS (재귀)
-def dfs(v, visited):
-    visited[v] = True
-    print(v, end=' ')
-    for neighbor in graph[v]:
-        if not visited[neighbor]:
-            dfs(neighbor, visited)
+visited = [False] * (n+1)
+# dfs
+def dfs(cur):
+    visited[cur] = True
+    print(cur, end=' ')
+    
+    for nxt in graph[cur]:
+        if not visited[nxt]:
+            dfs(nxt)
 
-# BFS (큐)
-def bfs(start):
-    visited = [False] * (n + 1)
-    queue = deque([start])
-    visited[start] = True
+# bfs
+def bfs(graph, cur_v):
+    q = deque()
+    q.append(v)
+    visited = {v: True}
+    while q:
+        cur_v = q.popleft()
+        print(cur_v, end=' ')
+        for next_v in graph[cur_v]:
+            if next_v not in visited:
+                q.append(next_v)
+                visited[next_v] = True
+    return
 
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for neighbor in graph[v]:
-            if not visited[neighbor]:
-                queue.append(neighbor)
-                visited[neighbor] = True
-
-# 실행
-visited_dfs = [False] * (n + 1)
-dfs(v, visited_dfs)
-print()
-bfs(v)
+dfs(v)
+print("")
+bfs(graph,v)
