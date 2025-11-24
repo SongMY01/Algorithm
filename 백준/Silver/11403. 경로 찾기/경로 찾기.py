e4e7256531d12216e9a25c3c3d7998ef3e3ad248
MYONE <132147530+SongMY01@users.noreply.github.com>
2025-11-24 23:23:46 +0900

    [Silver I] Title: 경로 찾기, Time: 220 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+# 플로이드 워셜 (경로 존재 판정)
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+# 출력
+for row in graph:
+    print(*row)
